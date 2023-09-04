988e91c142040018ed6163cc73e17e3a4fcac3b2
crkur <ckurdelak20@georgefox.edu>
2023-09-03 23:47:12 -0700

    header comment

--- stats.f90
+++ stats.f90
@@ -1,1 +1,5 @@
-
+!---------------------------------------------------------------------------------------------------
+! stats.f90
+! ckurdelak20@georgefox.edu
+! Reads exactly 10 floating point values from a file and computes and prints their statistics
+!---------------------------------------------------------------------------------------------------
