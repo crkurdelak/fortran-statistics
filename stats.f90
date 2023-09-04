154b2dc6a99c54c320433a06f436260c47b3fc9c
crkur <ckurdelak20@georgefox.edu>
2023-09-04 16:02:45 -0700

    start calculations

--- stats.f90
+++ stats.f90
@@ -5,12 +5,21 @@
 !---------------------------------------------------------------------------------------------------
 PROGRAM stats
     ! define vars
-    ! create array of 10 floats
-    REAL vals(0:9)
+    REAL vals(0:9)                                  ! create array of 10 floats
+    REAL :: val_mean, val_min, val_max, val_std     ! create vars for stats
+    CHARACTER*10 :: format_string = '(A, F64.2)'    ! 10-character format string for output
 
-    REAL :: val_mean, val_min, val_max, val_std
+    ! TODO get values from grades.dat
+    ! compute stats
+    ! val_mean = mean(vals)
+    val_min = MIN(vals(0), vals(1), vals(2), vals(3), vals(4), vals(5), vals(6), vals(7), vals(8), vals(9))
+    val_max = MAX(vals(0), vals(1), vals(2), vals(3), vals(4), vals(5), vals(6), vals(7), vals(8), vals(9))
+    ! val_std = std(vals)
+    ! TODO define mean and stdev functions
 
-    ! TODO get values from file
-    ! TODO compute stats
-    ! TODO print results
+    ! print results
+    WRITE(*, format_string) 'Mean:', val_mean
+    WRITE(*, format_string) 'Min:', val_min
+    WRITE(*, format_string) 'Max:', val_max
+    WRITE(*, format_string) 'Std Dev:', val_std
 END PROGRAM stats
