008594e261a715d0f675596c97af0663b2c024c4
crkur <ckurdelak20@georgefox.edu>
2023-09-04 10:28:31 -0700

    define vars

--- stats.f90
+++ stats.f90
@@ -3,3 +3,14 @@
 ! ckurdelak20@georgefox.edu
 ! Reads exactly 10 floating point values from a file and computes and prints their statistics
 !---------------------------------------------------------------------------------------------------
+PROGRAM stats
+    ! define vars
+    ! create array of 10 floats
+    REAL vals(0:9)
+
+    REAL :: val_mean, val_min, val_max, val_std
+
+    ! TODO get values from file
+    ! TODO compute stats
+    ! TODO print results
+END PROGRAM stats
